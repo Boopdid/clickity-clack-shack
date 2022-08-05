cc2f1ff778238426f07d209267dfebaf8260f949
Peter Vrutneski <pvrutneski@gmail.com>
2022-08-05 15:10:05 -0600

    Move import

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
+import User from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
